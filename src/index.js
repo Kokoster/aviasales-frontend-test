39e15c8580cc944b96f6254ad6e629aa2c5f95e7
Kokoster <dokokoster9@gmail.com>
2018-12-19 23:19:14 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App.jsx';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(
+	() => {},
+	window.__REACT_DEVTOOLS_EXTENSION__ && window.__REACT_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
+
+serviceWorker.unregister();
